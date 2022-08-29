7de33e77d63efee87d0d55fd09106ecfbe06ae67
Principle <92553079+principlebrothers@users.noreply.github.com>
2022-08-29 22:27:30 +0000

    Add component folder with Countries, Cities and States components

---
+++ src/components/countries.js
@@ -0,0 +1,13 @@
+import React from "react";
+import States from "./states";
+
+const Countries = () => {
+  return (
+    <div className="countryContainer">
+      <div>Countries</div>
+      <States />
+    </div>
+  );
+};
+
+export default Countries;
---
+++ src/components/states.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Cities from './Cities';
+
+const States = () => {
+  return (
+    <div className='stateContainer'>
+      <div>States</div>
+      <Cities />
+    </div>
+  )
+}
+
+export default States;

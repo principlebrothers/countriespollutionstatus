d2117c63b169baa1ba82890416115264bb97dd0d
Principle <92553079+principlebrothers@users.noreply.github.com>
2022-08-31 08:37:46 +0000

    Add action creators

--- src/redux/countries/countries.js
+++ src/redux/countries/countries.js
@@ -1,21 +1,36 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import creatAsyncThunk from '@reduxjs/toolkit';
 
-FETCH_DATA = 'secnewshub/countries/FETCH_DATA';
+const FETCH_DATA = 'countriespollutionstatus/countries/FETCH_DATA';
+const GET_DEATILS = 'countriespollutionstatus/countries/GET_DEATILS';
 
-initialState = {
-  countries: [],
-  loading: false,
-  error: null,
-};
+const initialState = [];
 
 const countriesReducer = (state = initialState, action) => {
   switch (action.type) {
     case `${FETCH_DATA}/fulfilled`:
       return action.payload;
+    case `${FETCH_DATA}/rejected`:
+      return action.payload;
+    case GET_DEATILS:
+      return action.payload;
     default:
       return state;
-  };
+  }
 };
+
+export const getStatistics = (country) => ({
+  type: GET_DEATILS,
+  payload: country,
+});
+
+export const fetchData = createAsyncThunk(FETCH_DATA, async () => {
+  try {
+    const { data } = await axios.get('https://api.covid19api.com/summary');
+    return data.Countries;
+  } catch (error) {
+    return error;
+  }
+});
 
 export default countriesReducer;

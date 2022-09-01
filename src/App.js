1812c9b1b97630e94e237a1030c8a24e410d2456
Principle <92553079+principlebrothers@users.noreply.github.com>
2022-09-01 13:09:54 +0000

    Add logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Countries from './components/countries';
+import logo from './assets/logo.png';
 import Statistics from './components/countryStatistics';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Router>
-          <h1>Countries</h1>
-          <Routes>
-            <Route path="/" element={<Countries />} />
-            <Route path="/statistics" element={<Statistics />} />
-          </Routes>
-        </Router>
-      </header>
+const App = () => (
+  <div className="App">
+    <div className="header">
+      <div className="logoContainer">
+        <img
+          style={{ width: 220, height: 100 }}
+          className="logo"
+          src={logo}
+          alt="logo"
+        />
+      </div>
     </div>
-  );
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Countries />} />
+        <Route path="/statistics/:country" element={<Statistics />} />
+      </Routes>
+    </Router>
+  </div>
+);
 
 export default App;
